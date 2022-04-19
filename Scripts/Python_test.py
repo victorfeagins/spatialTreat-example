fcd3492c66bed595a4834a5d786046992390c06a
Victor Feagins <victorfeagins@att.net>
2022-04-19 10:51:45 -0500

    Testing Conda Python in HPC

---
+++ Scripts/Python_test.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from math import sin, cos, pi  # rotating regions
+from math import floor  # truncating naics codes
+import numba  # speed up data transform with JIT compilation
